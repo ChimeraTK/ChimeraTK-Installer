3f10c7c22f37b2ce8b05b0ac375a0831d691026e
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2014-11-09 23:12:12 +0000

    fixed version number of CommandLineTools for mtca4u version 00.15.00
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/mtca4u_installer/trunk@641 5f601a5c-88b9-47f3-b851-818f6741d00d

--- cmakemodules/MTCA4U_VERSION_00.15.00.cmake
+++ cmakemodules/MTCA4U_VERSION_00.15.00.cmake
@@ -2,7 +2,7 @@
 set(MtcaMappedDevice_VERSION "00.12.00")
 set(QtHardMon_VERSION "00.08.00")
 set(MotorDriverCard_VERSION "00.04.00")
-set(CommandLineTools_VERSION "00.01.00")
+set(CommandLineTools_VERSION "00.01.01")
 
 #the dependencies with their minimal versions
 set(pugixml_MIN_VERSION "1.3")
