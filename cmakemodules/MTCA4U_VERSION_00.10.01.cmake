6b4bbe1fee2802e416d6a8a40bc62cd0c8e1e5d9
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-03-20 15:53:26 +0000

    Backport tags 00.09.03 00.10.01 00.11.01 00.12.01 00.13.01 00.14.01  00.15.01 00.16.02
    - All versions with pugixml dependency have been updated to pugixml 1.4,
      incl. installer for the HEAD (although the trunk should be used in this case)
    
    
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/mtca4u_installer/tags/00.10.01@874 5f601a5c-88b9-47f3-b851-818f6741d00d

--- cmakemodules/MTCA4U_VERSION_00.10.01.cmake
+++ cmakemodules/MTCA4U_VERSION_00.10.01.cmake
@@ -1,7 +1,7 @@
 #parts of mtca4u with their versions
-set(MtcaMappedDevice_VERSION "HEAD")
-set(QtHardMon_VERSION "HEAD")
-set(MotorDriverCard_VERSION "HEAD")
+set(MtcaMappedDevice_VERSION "00.07.00")
+set(QtHardMon_VERSION "00.05.01")
+set(MotorDriverCard_VERSION "00.02.01")
 
 #the dependencies with their minimal versions
 set(pugixml_MIN_VERSION "1.4")
