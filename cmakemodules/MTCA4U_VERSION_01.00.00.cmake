b0fe1069d1160f725c61eb9a7f2ebf5921183cfc
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-01-15 22:40:10 +0000

    - final adjustments of the versions in 01.00.00 - again undid the accidental change in CMakeLists.txt in the last commit
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/mtca4u_installer/trunk@2071 5f601a5c-88b9-47f3-b851-818f6741d00d

--- cmakemodules/MTCA4U_VERSION_01.00.00.cmake
+++ cmakemodules/MTCA4U_VERSION_01.00.00.cmake
@@ -1,7 +1,7 @@
 #parts of mtca4u with their versions
 set(mtca4u-deviceaccess_VERSION "00.04.01")
 set(QtHardMon_VERSION "00.15.02")
-set(MotorDriverCard_VERSION "00.07.03")
+set(MotorDriverCard_VERSION "00.07.04")
 set(CommandLineTools_VERSION "00.08.00")
 set(mtca4uPy_VERSION "00.04.02")
 
