4d1af572a45d7028adb080d00b502ffbdc6d6e5a
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-01-18 13:40:44 +0000

    Changes for creating new tag: 01.00.01
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/mtca4u_installer/trunk@2093 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ cmakemodules/MTCA4U_VERSION_01.00.01.cmake
@@ -0,0 +1,9 @@
+#parts of mtca4u with their versions
+set(mtca4u-deviceaccess_VERSION "00.04.01")
+set(QtHardMon_VERSION "00.15.02")
+set(MotorDriverCard_VERSION "00.07.06")
+set(CommandLineTools_VERSION "00.08.01")
+set(mtca4uPy_VERSION "00.04.04")
+
+#the dependencies with their minimal versions
+set(pugixml_MIN_VERSION "1.4")
