10e62158d3b9b8a2316272335976f25a588913d4
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-02-18 09:22:26 +0000

    added missing changes in HEAD version file
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/mtca4u_installer/trunk@2293 5f601a5c-88b9-47f3-b851-818f6741d00d

--- cmakemodules/MTCA4U_VERSION_HEAD.cmake
+++ cmakemodules/MTCA4U_VERSION_HEAD.cmake
@@ -4,6 +4,7 @@ set(QtHardMon_VERSION "HEAD")
 set(MotorDriverCard_VERSION "HEAD")
 set(CommandLineTools_VERSION "HEAD")
 set(mtca4uPy_VERSION "HEAD")
+set(mtca4uVirtualLab_VERSION "HEAD")
 
 #the dependencies with their minimal versions
 set(pugixml_MIN_VERSION "1.4")
