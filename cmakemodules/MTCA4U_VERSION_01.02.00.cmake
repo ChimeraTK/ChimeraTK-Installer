39aca761e7932d07545a0d9062a53725e050fd46
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-03-02 09:13:07 +0000

    still patching version numbers for the release
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/mtca4u_installer/trunk@2411 5f601a5c-88b9-47f3-b851-818f6741d00d

--- cmakemodules/MTCA4U_VERSION_01.02.00.cmake
+++ cmakemodules/MTCA4U_VERSION_01.02.00.cmake
@@ -2,7 +2,7 @@
 set(mtca4u-deviceaccess_VERSION "00.07.01")
 set(QtHardMon_VERSION "00.16.00")
 set(MotorDriverCard_VERSION "00.08.01")
-set(CommandLineTools_VERSION "00.09.01")
+set(CommandLineTools_VERSION "00.09.02")
 set(mtca4uPy_VERSION "00.05.00")
 set(mtca4uVirtualLab_VERSION "00.02.01")
 
