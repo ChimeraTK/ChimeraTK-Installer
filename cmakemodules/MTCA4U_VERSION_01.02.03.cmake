2fbf9c65cb2497c7401bf7f0e2b8ed4530402fb9
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-03-17 22:04:42 +0000

    need even later patches of the dependent libraries due to debian problems (packages not installable)
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/mtca4u_installer/trunk@2527 5f601a5c-88b9-47f3-b851-818f6741d00d

--- cmakemodules/MTCA4U_VERSION_01.02.03.cmake
+++ cmakemodules/MTCA4U_VERSION_01.02.03.cmake
@@ -1,10 +1,10 @@
 #parts of mtca4u with their versions
 set(mtca4u-deviceaccess_VERSION "00.08.01")
 set(QtHardMon_VERSION "00.16.00")
-set(MotorDriverCard_VERSION "00.08.02")
+set(MotorDriverCard_VERSION "00.08.03")
 set(CommandLineTools_VERSION "00.09.02")
 set(mtca4uPy_VERSION "00.05.00")
-set(mtca4uVirtualLab_VERSION "00.02.02")
+set(mtca4uVirtualLab_VERSION "00.02.03")
 
 #the dependencies with their minimal versions
 set(pugixml_MIN_VERSION "1.4")
