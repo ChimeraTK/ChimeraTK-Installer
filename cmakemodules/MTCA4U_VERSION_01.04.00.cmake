760c03b6b3e8a09c39b8386a713c5c83ebdefb13
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-04-13 10:00:44 +0000

    added preliminary version of the 01.04.00 version file
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/mtca4u_installer/trunk@2694 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ cmakemodules/MTCA4U_VERSION_01.04.00.cmake
@@ -0,0 +1,10 @@
+#parts of mtca4u with their versions
+set(mtca4u-deviceaccess_VERSION "00.08.02")
+set(QtHardMon_VERSION "00.17.02")
+set(MotorDriverCard_VERSION "00.09.01")
+set(CommandLineTools_VERSION "00.10.00")
+set(mtca4uPy_VERSION "00.06.00")
+set(mtca4uVirtualLab_VERSION "00.02.03")
+
+#the dependencies with their minimal versions
+set(pugixml_MIN_VERSION "1.4")
