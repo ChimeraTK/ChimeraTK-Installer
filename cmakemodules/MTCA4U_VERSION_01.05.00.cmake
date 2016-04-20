245d5b2d3ad4a3e09730fb2ebbc6d02255747a0b
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-04-20 15:06:12 +0000

    prepared tag 01.05.00
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/mtca4u_installer/trunk@2753 5f601a5c-88b9-47f3-b851-818f6741d00d

--- cmakemodules/MTCA4U_VERSION_01.05.00.cmake
+++ cmakemodules/MTCA4U_VERSION_01.05.00.cmake
@@ -1,5 +1,5 @@
 #parts of mtca4u with their versions
-set(mtca4u-deviceaccess_VERSION "00.09.00")
+set(mtca4u-deviceaccess_VERSION "00.10.00")
 set(QtHardMon_VERSION "00.17.02")
 set(MotorDriverCard_VERSION "00.09.01")
 set(CommandLineTools_VERSION "00.10.00")
