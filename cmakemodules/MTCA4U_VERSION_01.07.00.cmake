33cbacd6bcbb66bc3865877ec1ac77fe6907e2ee
Martin Killenberg <martin.killenberg@desy.de>
2016-07-29 11:41:39 +0200

    increased patch level versions of DeviceAccess and VirtualLab for the comming 01.07.00 release

--- cmakemodules/MTCA4U_VERSION_01.07.00.cmake
+++ cmakemodules/MTCA4U_VERSION_01.07.00.cmake
@@ -1,8 +1,8 @@
 #parts of mtca4u with their versions
-set(mtca4u-deviceaccess_VERSION "00.12.02")
+set(mtca4u-deviceaccess_VERSION "00.12.03")
 set(QtHardMon_VERSION "00.17.04")
 set(MotorDriverCard_VERSION "00.11.06")
 set(CommandLineTools_VERSION "00.11.00")
 set(mtca4uPy_VERSION "00.08.00")
-set(mtca4uVirtualLab_VERSION "00.03.02")
+set(mtca4uVirtualLab_VERSION "00.03.03")
 
