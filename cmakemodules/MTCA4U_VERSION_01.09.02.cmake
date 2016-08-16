31d184d139f047f8f7e5ca2ffd6db57eb21a162e
Martin Killenberg <martin.killenberg@desy.de>
2016-08-16 18:46:46 +0200

    prepared 01.09.02

---
+++ cmakemodules/MTCA4U_VERSION_01.09.02.cmake
@@ -0,0 +1,8 @@
+#parts of mtca4u with their versions
+set(mtca4u-deviceaccess_VERSION "00.15.01")
+set(QtHardMon_VERSION "00.17.07")
+set(MotorDriverCard_VERSION "00.12.01")
+set(CommandLineTools_VERSION "00.11.02")
+set(mtca4uPy_VERSION "00.08.02")
+set(mtca4uVirtualLab_VERSION "00.03.05")
+
