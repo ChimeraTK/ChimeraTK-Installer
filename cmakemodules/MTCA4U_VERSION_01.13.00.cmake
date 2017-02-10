25e79ceabcd2d75754ac6055a6cfaa41fff6d666
Martin Killenberg <martin.killenberg@desy.de>
2017-02-10 14:38:57 +0100

    prepared version 01.13.00 (update to DeviceAccess 00.20.00)

---
+++ cmakemodules/MTCA4U_VERSION_01.13.00.cmake
@@ -0,0 +1,8 @@
+#parts of mtca4u with their versions
+set(mtca4u-deviceaccess_VERSION "00.20.00")
+set(QtHardMon_VERSION "00.18.01")
+set(MotorDriverCard_VERSION "00.14.00")
+set(CommandLineTools_VERSION "00.12.00")
+set(mtca4uPy_VERSION "00.09.01")
+set(mtca4uVirtualLab_VERSION "00.05.00")
+set(MotorDriverCard-PythonBindings_VERSION "00.03.02")
