0bd53284e47bfd917a482f2be651d572bf277292
Martin Killenberg <martin.killenberg@desy.de>
2017-12-07 16:31:34 +0100

    fixed QtHardMon version

--- cmakemodules/MTCA4U_VERSION_01.14.00.cmake
+++ cmakemodules/MTCA4U_VERSION_01.14.00.cmake
@@ -1,8 +1,9 @@
 #parts of mtca4u with their versions
 set(mtca4u-deviceaccess_VERSION "00.28.00")
-set(QtHardMon_VERSION "00.18.01")
+set(QtHardMon_VERSION "00.18.02")
 set(MotorDriverCard_VERSION "01.02.01")
 set(CommandLineTools_VERSION "00.12.01")
 set(mtca4uPy_VERSION "01.01.01")
 set(mtca4uVirtualLab_VERSION "00.06.00")
 set(MotorDriverCard-PythonBindings_VERSION "00.03.02")
+#set(ControlSystemAdapter_VERSION "00.10.00")
