facd2aa4ea0a98853c62ac2bec97d6317d79d32b
Martin Killenberg <martin.killenberg@desy.de>
2018-09-19 15:04:50 +0200

    added config for 01.16.00. Not used yet

---
+++ cmakemodules/MTCA4U_VERSION_01.16.00.cmake
@@ -0,0 +1,12 @@
+#parts of mtca4u with their versions
+set(cppext_VERSION "01.01.00")
+set(mtca4u-deviceaccess_VERSION "01.00.04")
+set(QtHardMon_VERSION "01.01.00")
+set(MotorDriverCard_VERSION "01.07.00")
+set(CommandLineTools_VERSION "00.13.00")
+set(mtca4uPy_VERSION "01.02.00")
+set(mtca4uVirtualLab_VERSION "00.07.00")
+set(MotorDriverCard-PythonBindings_VERSION "00.03.02")
+set(ControlSystemAdapter_VERSION "01.00.00")
+set(ControlSystemAdapter-OPC-UA-Adapter_VERSION "01.06.00")
+set(ApplicationCore_VERSION "01.00.00")
